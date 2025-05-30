5d2090d6fc5c26b52749e44a66585785de8e98eb
kavancamp <keenahvancampenhout@gmail.com>
2025-05-29 20:09:49 -0500

    updated practice tab

--- window.py
+++ window.py
@@ -30,7 +30,10 @@ class FlashcardApp:
         self.definition = tk.StringVar()
 
         init_db()
-
+        
+        self.current_cards = []
+        self.card_index = 0
+        
         # persistent header above the notebook
         self.header_canvas = tk.Canvas(
             self.root,
@@ -78,14 +81,14 @@ class FlashcardApp:
             btn.bind("<Leave>", lambda e, i=index: self.on_leave(i))
             btn.bind("<Button-1>", lambda e, i=index: self.select_tab(i))
             self.tab_buttons.append(btn)
-
+            
         # Create content area
         self.content_frame = tk.Frame(self.root, bg="white")
         self.content_frame.pack(fill="both", expand=True)
-
         
         # Select first tab by default
         self.select_tab(0)
+        
         self.root.mainloop()
         
     def on_hover(self, index):
@@ -141,10 +144,10 @@ class FlashcardApp:
             image=self.button_image_save_word,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: [print("saving input..."), self.save_word()],
+            command=lambda: [print("saving word..."), self.save_word()],
             relief="flat"
         )
-        save_word_button.place(x=130, y=270, width=151, height=69)
+        save_word_button.place(x=50, y=270, width=151, height=69)
         save_word_button.bind("<Enter>", lambda e: save_word_button.config(image=self.button_hover_save_word))
         save_word_button.bind("<Leave>", lambda e: save_word_button.config(image=self.button_image_save_word)) 
         
@@ -152,42 +155,83 @@ class FlashcardApp:
         self.button_image_save_set = PhotoImage(file=relative_to_assets("new_set_images/save_set.png"))
         self.button_hover_save_set = PhotoImage(file=relative_to_assets("new_set_images/save_set_hover.png"))
 
-        save_set_button= Button(
+        save_set_button = Button(
             self.content_frame,
             image=self.button_image_save_set,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: [print("saving input..."), self.save_set()],
+            command=lambda: [print("saving set..."), self.save_set()],
             relief="flat"
         )
-        save_set_button.place(x=130, y=270, width=151, height=69)
+        save_set_button.place(x=200, y=270, width=151, height=69)
         save_set_button.bind("<Enter>", lambda e: save_set_button.config(image=self.button_hover_save_set))
         save_set_button.bind("<Leave>", lambda e: save_set_button.config(image=self.button_image_save_set)) 
-    
-        
+       
     def load_my_sets(self):
-        tk.Label(self.content_frame, text="My Sets Screen", bg="white").pack()
+        tk.Label(self.content_frame, text="My Sets", bg="light gray").pack()
         # Combobox  for selecting card sets
-        sets_dropdown = ttk.Combobox(self.content_frame, state='readonly')
-        sets_dropdown.pack(padx=5, pady=40)
-
-
-    def load_practice(self):
-        tk.Label(self.content_frame, text="Practice Screen", bg="white").pack() 
+        self.sets_combobox = ttk.Combobox(self.content_frame, state='readonly', width=30)
+        self.sets_combobox.pack(padx=5, pady=50)
+        self.fill_combobox()
         
+        self.button_image_select = PhotoImage(file=relative_to_assets("my_set_images/select.png"))
+        self.button_hover_select = PhotoImage(file=relative_to_assets("my_set_images/select_hover.png"))
+        select_button = Button(
+            self.content_frame,
+            image=self.button_image_select,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: [print("loading practice screen..."), self.select_set()],
+            relief="flat"
+        )
+        select_button.place(x=50.0, y=200.0, width=151.0, height=69.0)
+        select_button.bind("<Enter>", lambda e: select_button.config(image=self.button_hover_select))
+        select_button.bind("<Leave>", lambda e: select_button.config(image=self.button_image_select))
+        
+        self.button_image_delete = PhotoImage(file=relative_to_assets("my_set_images/delete.png"))
+        self.button_hover_delete = PhotoImage(file=relative_to_assets("my_set_images/delete_hover.png"))  
+        delete_button = Button(
+            self.content_frame,
+            image=self.button_image_delete,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: [print("Deleting set..."), self.delete_selected_set()],
+            relief="flat"
+        )
+        delete_button.place(x=210, y=200.0, width=151.0, height=69.0)
+        delete_button.bind('<Enter>', lambda e: delete_button.config(image=self.button_hover_delete))
+        delete_button.bind('<Leave>', lambda e: delete_button.config(image=self.button_image_delete))
+        
+    def load_practice(self, show_first_card=False):
+        self.card_bg = PhotoImage(file=relative_to_assets("practice_images/card_background.png"))
+        card_image = tk.Label(self.content_frame, image=self.card_bg, bg="white")
+        card_image.place(x = 20, y=10)
+        # Initialize variables for tracking card index and current cards
+        self.card_index = 0
+        self.current_tabs = []
+
+        # # Label to display the word on flashcards
+        self.word_label = tk.Label(self.content_frame, text='word', bg="#F1F5FF", font=('TkDefaultFont', 24))
+        self.word_label.pack(padx=5, pady=20)
+        
+        # # Label to display the definition on flashcards
+        self.definition_label = tk.Label(self.content_frame,  text='definition', bg="#F1F5FF")
+        self.definition_label.pack(padx=5, pady=30)
+        tk.Label(self.content_frame, text="Practice Area", bg="#F1F5FF", font=(self.custom_font, 10)).pack()
+
+        # Button to flip the flashcard 
         self.button_image_flip = PhotoImage(file=relative_to_assets("practice_images/flip.png"))
         self.button_hover_flip = PhotoImage(file=relative_to_assets("practice_images/flip_hover.png"))
         flip_button = Button(
-            self.content_frame,
             image=self.button_image_flip,
             borderwidth=0,
             highlightthickness=0,
-            #command=lambda: self.load_practice_screen,
+            command=lambda: [print("Flipping card..."), self.flip_card()],
             relief="flat"
         )
         flip_button.place(
             x=56.0,
-            y=411.0,
+            y=405.0,
             width=137.0,
             height=55.0
         )
@@ -198,18 +242,14 @@ class FlashcardApp:
         self.button_hover_next = PhotoImage(file=relative_to_assets("practice_images/next_hover.png"))
         
         next_button = Button(
-            self.content_frame,
             image=self.button_image_next,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: print("Next clicked"),
+            command=lambda: [print("Flipping card..."), self.next_card()],
             relief="flat"
         )       
         next_button.place(
-            x=230.0,
-            y=411.0,
-            width=137.0,
-            height=55.0
+            x=230.0, y=405.0, width=137.0, height=55.0
         )
         next_button.bind('<Enter>', lambda e: next_button.config(image=self.button_hover_next))
         next_button.bind('<Leave>', lambda e: next_button.config(image=self.button_image_next))
@@ -219,185 +259,132 @@ class FlashcardApp:
         self.button_hover_back = PhotoImage(file=relative_to_assets("practice_images/back_hover.png"))
 
         back_button = Button(
-            self.content_frame,
+
             image=self.button_image_back,
             borderwidth=0,
             highlightthickness=0,
-            command=lambda: print("back_button button_6 clicked"),
+            command=lambda: [print("Flipping card..."), self.prev_card],
             relief="flat"
         )
         back_button.place(
-            x=140.0,
-            y=472.0,
-            width=147.0,
-            height=61.0
+            x=140.0, y=460.0, width=147.0, height=61.0
         )
-        
-        # self.canvas.create_rectangle(
-        #     11.0,
-        #     138.0,
-        #     411.0,
-        #     401.0,
-        #     fill="#FFFFFF",
-        #     outline="")
+        back_button.bind('<Enter>', lambda e: back_button.config(image=self.button_hover_back))
+        back_button.bind('<Leave>', lambda e: back_button.config(image=self.button_image_back))
 
-        card_bg = PhotoImage(
-            file=relative_to_assets("practice_images/card_background.png"))
-        image_1 = self.canvas.create_image(
-            211.0,
-            269.0,
-            image=self.card_bg
-        )
-        self.canvas.create_text(
-            65.0,
-            180.0,
-            anchor="nw",
-            text="card text goes here",
-            fill="#000000",
-            font=(self.custom_font, 16 * -1)
-        )
+
+        if show_first_card:
+            self.show_card()
        
-        
-        
-    def populate_combobox(self):
-        self.sets_dropdown['values'] = tuple(get_stacks().keys()) 
+    def fill_combobox(self):
+        if hasattr(self, 'sets_combobox'):
+            self.sets_combobox['values'] = tuple(get_stacks().keys())   
            
     def save_word(self):
-        stack_name = stack_name.get()
-        word = word.get()
-        definition = definition.get()     
+        stack_name = self.stack_name.get().strip()
+        word = self.word.get().strip()
+        definition = self.definition.get().strip()
+
         if stack_name and word and definition:
             if stack_name not in get_stacks():
-                set_id = add_new_stack(stack_name)
+                stack_id = add_new_stack(stack_name)
             else:
-                set_id = get_stacks()[stack_name]
+                stack_id = get_stacks()[stack_name]
 
-            add_new_card(set_id, word, definition)
+            add_new_card(stack_id, word, definition)
 
-            word.set('')
-            definition.set('')
+            self.word.set('')
+            self.definition.set('')
 
-            self.populate_combobox()
-                
+            self.fill_combobox() 
+                       
     def save_set(self):
         stack_name = self.stack_name.get().strip()
         if stack_name:
             if stack_name not in get_stacks():
                 stack_id = add_new_stack(stack_name)
-                self.populate_combobox()
-                stack_name.set('')
+                self.fill_combobox()
+                self.stack_name.set('')
                 print(f"Saved word: {self.word} with definition: {self.definition} to set: {self.stack_name}")
                 
                 self.entry_1.delete(0, tk.END)
                 self.entry_2.delete(0, tk.END)
                 self.entry_3.delete(0, tk.END)
-    
-    # Function to create a new flashcard set
-    def create_set(self):
-        stack_name = self.stack_name.get().strip()
+                
+    def delete_selected_set(self):
+        stack_name = self.sets_combobox.get()
         if stack_name:
-            if stack_name not in get_stacks():
-                set_id = add_new_stack(stack_name)
-                self.populate_combobox()
-                stack_name.set('')
-
-                # Clear the input fields
-                self.stack_name.set('')
-                self.word.set('')
-                self.definition.set('')
-                
-    def add_word(self):
-        set_name = self.set_name.get().strip()
-        word = self.word.get().strip()
-        definition = self.definition.get().strip()
-
-        if set_name and word and definition:
-            if set_name not in self.get_sets():
-                set_id = self.add_new_set(set_name)
-            else:
-                set_id = self.get_sets()[set_name]
-
-            self.add_card(set_id, word, definition)
-
-            self.word.set('')
-            self.definition.set('')
-
-            self.populate_combobox()
-                
+            result = messagebox.askyesno(
+                'Confirmation', f'Delete "{stack_name}" set?')
+            if result == tk.YES:
+                stack_id = get_stacks()[stack_name]
+                delete_set(stack_id)  
+                self.fill_combobox()  # GUI update
+                #self.clear_flashcard_display()  # GUI update       
+                     
     def select_set(self):
-        set_name = self.sets_dropdown.get()
-
-        if set_name:
-            set_id = self.get_sets()[self.stack_name]
-            cards = self.get_cards(set_id)
+        stack_name = self.sets_combobox.get().strip()
+        if stack_name:
+            stack_id = get_stacks()[stack_name]
+            cards = get_cards(stack_id)
 
             if cards:
-                self.display_flashcards(cards)
+                self.tabs[2]["command"] = lambda: self.load_practice(show_first_card=True)
+                random.shuffle(cards)
+                self.current_cards = cards
+                self.card_index = 0
+                self.select_tab(2)
+                self.show_card()
+                self.display_cards(cards)
+                print(f"{cards}")
             else:
-                self.word_label.config(text="No cards in this set")
-                self.definition_label.config(text='')
+                messagebox.showinfo("Info", "This set has no cards.")
         else:
-            # Clear the current cards list and reset card index
-            global current_cards, card_index
-            current_cards = []
-            card_index = 0
+            messagebox.showwarning("Warning", "Please select a set.")
+  
+    def display_cards(self, cards):
+        self. card_index = 0
+        self.current_cards = cards
+        
+        # Clear the display
+        if not cards:
             self.clear_flashcard_display()
-            
-    def delete_selected_set(self):
-        set_name = self.sets_dropdown.get()
-        if set_name:
-            result = messagebox.askyesno(
-                'Confirmation', f'Are you sure you want to delete the "{set_name}" set?'
-            )
-            if result == tk.YES:
-                set_id = get_stacks()[set_name]
-                delete_set(set_id)  # from database.py
-                self.populate_combobox()  # GUI update
-                self.clear_flashcard_display()  # GUI update
-            
+        else:
+            self.show_card()
+        
+        self.show_card()
+
     def clear_flashcard_display(self):
-        self.word_label.config(text='')
-        self.definition_label.config(text='')
+        if hasattr(self, 'word_label') and self.word_label.winfo_exists():
+            self.word_label.config(text='')
+        if hasattr(self, 'definition_label') and self.definition_label.winfo_exists():
+            self.definition_label.config(text='')
 
     # Function to display the current flashcards word
     def show_card(self):
-        global card_index
-        global current_cards
-
-        if current_cards:
-            if 0 <= card_index < len(current_cards):
-                word, _ = current_cards[card_index]
+        if self.current_cards:
+            if 0 <= self.card_index < len(self.current_cards):
+                word, _ = self.current_cards[self.card_index]
                 self.word_label.config(text=word)
                 self.definition_label.config(text='')
             else:
                 self.clear_flashcard_display()
-        else:
-            self.clear_flashcard_display()
 
     # flip the current card and display its definition
     def flip_card(self):
-        global card_index
-        global current_cards
-
-        if current_cards:
-            _, definition = current_cards[card_index]
+        if self.current_cards:
+            _, definition = self.current_cards[self.card_index]
             self.definition_label.config(text=definition)
 
     # Function to move to the next card
     def next_card(self):
-        global card_index
-        global current_cards
-
-        if current_cards:
-            card_index = min(card_index + 1, len(current_cards) -1)
+        if self.current_cards:
+            self.card_index = min(self.card_index + 1, len(self.current_cards) - 1)
             self.show_card()
 
     # Function to move to the previous card
     def prev_card(self):
-        global card_index
-        global current_cards
-
-        if current_cards:
-            card_index = max(card_index - 1, 0)
+        if self.current_cards:
+            self.card_index = max(self.card_index - 1, 0)
             self.show_card()
-        
+            
